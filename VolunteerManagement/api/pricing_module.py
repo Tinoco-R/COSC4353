33078da278a061ac7d5c305aafec83ba74083d54
axel-ra <ax.alvarenga01@gmail.com>
2024-07-09 21:45:07 -0500

    Pricing Module Commit -Added the Pricing Module (only a class)

---
+++ VolunteerManagement/api/pricing_module.py
@@ -0,0 +1,32 @@
+# Pricing Module (class only)
+
+class PricingModule():
+    data1 = None
+    data2 = None
+    data3 = None
+
+    def __init__(self):
+        self.data1 = 1 # Just assignig any value to the variables for now
+        self.data2 = 2
+        self.data3 = 3
+
+    def method1(self):
+        pass
+        return None
+    
+    def method2(self):
+        pass
+        return None
+    
+    def method2(self):
+        pass
+        return None
+    
+    def get_data1(self):
+        return self.data1
+    
+    def get_data2(self):
+        return self.data2
+    
+    def get_data2(self):
+        return self.data3
